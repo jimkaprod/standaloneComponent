58c87d033dab2eb80a0cd19bf8ba92bf760bbfd7
jimkaprod <jimkaprod@gmail.com>
2023-01-06 21:24:00 +0100

    3 files modified

--- src/app/mailbox/mailbox.component.ts
+++ src/app/mailbox/mailbox.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-mailbox',
   templateUrl: './mailbox.component.html',
-  styleUrls: ['./mailbox.component.css']
+  styleUrls: ['./mailbox.component.css'],
+  standalone: true,
+  imports: [RouterModule],
 })
 export class MailboxComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/mailbox/mailbox.routes.ts
+++ src/app/mailbox/mailbox.routes.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { ComposeComponent } from './compose/compose.component';
 import { ConversationComponent } from './conversation/conversation.component';
 import { ConversationsComponent } from './conversations/conversations.component';
+import { MailboxComponent } from './mailbox.component';
 import { MessageComponent } from './message/message.component';
 import { MessagesComponent } from './messages/messages.component';
 import { PopupMessageComponent } from './popup-message/popup-message.component';
@@ -9,34 +10,40 @@ import { PopupMessageComponent } from './popup-message/popup-message.component';
 export default [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: '/inbox',
-  },
-  {
-    path: ':folder',
+    component: MailboxComponent,
     children: [
       {
         path: '',
-        component: ConversationsComponent,
+        pathMatch: 'full',
+        redirectTo: '/mailbox/inbox',
       },
       {
-        path: ':id',
-        component: ConversationComponent,
+        path: ':folder',
         children: [
-          { path: 'messages', component: MessagesComponent },
-          { path: 'messages/:id', component: MessageComponent },
+          {
+            path: '',
+            component: ConversationsComponent,
+          },
+          {
+            path: ':id',
+            component: ConversationComponent,
+            children: [
+              { path: 'messages', component: MessagesComponent },
+              { path: 'messages/:id', component: MessageComponent },
+            ],
+          },
         ],
       },
+      {
+        path: 'compose',
+        component: ComposeComponent,
+        outlet: 'popup',
+      },
+      {
+        path: 'message/:id',
+        component: PopupMessageComponent,
+        outlet: 'popup',
+      },
     ],
-  },
-  {
-    path: 'compose',
-    component: ComposeComponent,
-    outlet: 'popup',
-  },
-  {
-    path: 'message/:id',
-    component: PopupMessageComponent,
-    outlet: 'popup',
   },
 ] as Route[];
